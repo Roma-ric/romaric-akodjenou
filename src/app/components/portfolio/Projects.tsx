9bb1942ae4d5319cd7db7ab75bf03b84fc1fa0d5
Romaric AKODJENOU <romaricakodjenou54@gmail.com>
2025-05-23 10:42:16 +0100

    23-05-2025 romaric

--- src/app/components/portfolio/Projects.tsx
+++ src/app/components/portfolio/Projects.tsx
@@ -8,36 +8,36 @@ const Projects = () => {
   const testimonials = [
     {
       quote:
-        "The attention to detail and innovative features have completely transformed our workflow. This is exactly what we've been looking for.",
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. non, modi consequuntur reprehenderit nulla praesentium.",
       name: "Sarah Chen",
       designation: "Product Manager at TechFlow",
       src: "/maquette/about-me2.png",
     },
     {
       quote:
-        "Implementation was seamless and the results exceeded our expectations. The platform's flexibility is remarkable.",
-      name: "Michael Rodriguez",
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. non, modi consequuntur reprehenderit nulla praesentium.",
+      name: "Lorem ipsum",
       designation: "CTO at InnovateSphere",
       src: "/maquette/about-me2.png",
     },
     {
       quote:
-        "This solution has significantly improved our team's productivity. The intuitive interface makes complex tasks simple.",
-      name: "Emily Watson",
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. non, modi consequuntur reprehenderit nulla praesentium.",
+      name: "Lorem ipsum",
       designation: "Operations Director at CloudScale",
       src: "/maquette/about-me2.png",
     },
     {
       quote:
-        "Outstanding support and robust features. It's rare to find a product that delivers on all its promises.",
-      name: "James Kim",
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. non, modi consequuntur reprehenderit nulla praesentium.",
+      name: "Lorem ipsum",
       designation: "Engineering Lead at DataPro",
       src: "/maquette/about-me2.png",
     },
     {
       quote:
-        "The scalability and performance have been game-changing for our organization. Highly recommend to any growing business.",
-      name: "Lisa Thompson",
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. non, modi consequuntur reprehenderit nulla praesentium.",
+      name: "Lorem ipsum",
       designation: "VP of Technology at FutureNet",
       src: "/maquette/about-me2.png",
     },
